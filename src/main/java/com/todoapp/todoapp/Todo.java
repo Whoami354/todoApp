33fff644dd397bce5feb6e7d69044c16c7908dbd
WhoAmI <fabian.gregory98@hotmail.de>
2026-02-22 10:49:17 +0100

    Klassen, Entitäten hinzugefuegt

---
+++ src/main/java/com/todoapp/todoapp/Todo.java
@@ -0,0 +1,41 @@
+package com.todoapp.todoapp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String description;
+    private boolean isDone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getIsDone() {
+        return isDone;
+    }
+
+    public void setIsDone(boolean done) {
+        this.isDone = done;
+    }
+}
---
+++ src/main/java/com/todoapp/todoapp/TodoController.java
@@ -0,0 +1,89 @@
+package com.todoapp.todoapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @GetMapping("/todo")
+    public ResponseEntity<Todo> get(@RequestParam(value = "id") int id)
+    {
+        // get todo from db by id
+        Optional<Todo> todoInDB = todoRepository.findById(id);
+
+        if(todoInDB.isPresent())
+        {
+            return new ResponseEntity<Todo>(todoInDB.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity("No todo found with id " + id, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/todo/all")
+    public ResponseEntity<Iterable<Todo>> getAll()
+    {
+        Iterable<Todo> allTodosInDb = todoRepository.findAll();
+        return new ResponseEntity<Iterable<Todo>>(allTodosInDb, HttpStatus.OK);
+    }
+
+    @PostMapping("/todo")
+    public ResponseEntity<Todo> create(@RequestBody Todo newTodo)
+    {
+        // save todo in db
+        todoRepository.save(newTodo);
+        return new ResponseEntity<Todo>(newTodo, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/todo")
+    public ResponseEntity delet(@RequestParam(value = "id") int id)
+    {
+        Optional<Todo> todoInDb = todoRepository.findById(id);
+        if(todoInDb.isPresent())
+        {
+            todoRepository.deleteById(id);
+            return new ResponseEntity("Todo deleted", HttpStatus.OK);
+        }
+
+        return new ResponseEntity("No todo to delete found with id " + id, HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/todo")
+    public ResponseEntity<Todo> edit(@RequestBody Todo editedTodo)
+    {
+        Optional<Todo> todoInDb = todoRepository.findById(editedTodo.getId());
+
+        if(todoInDb.isPresent())
+        {
+            // update
+            Todo savedTodo = todoRepository.save(editedTodo);
+            return new ResponseEntity<Todo>(savedTodo, HttpStatus.OK);
+        }
+        return new ResponseEntity("No todo to update found with id " + editedTodo.getId(), HttpStatus.NOT_FOUND);
+    }
+
+    @PatchMapping("/todo/setDone")
+    public ResponseEntity<Todo> setDone(@RequestParam(value = "isDone") boolean isDone,
+                                        @RequestParam(value = "id") int id)
+    {
+        Optional<Todo> todoInDb = todoRepository.findById(id);
+
+        if(todoInDb.isPresent())
+        {
+            // update isDone
+            todoInDb.get().setIsDone(isDone);
+            Todo savedTodo = todoRepository.save(todoInDb.get());
+            return new ResponseEntity<Todo>(savedTodo, HttpStatus.OK);
+        }
+
+        return new ResponseEntity("No todo to isDone found with id " + id, HttpStatus.NOT_FOUND);
+    }
+}
